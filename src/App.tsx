e7bf9e0bcc775010fa71cfdf50ab82928e8fc555
Tyler <rupert.jtyler@gmail.com>
2023-10-26 08:41:26 -0400

    Added secondary pages to header links

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <div className="text-center">
-        <Header />
-      </div>
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className="container">
+        <div className="text-center">
+          <Header />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About title={'About'} description={''} />} />
+          <Route path="/projects" element={<Projects title={'Projects'} description={''} />} />
+          <Route path="/contact" element={<Contact title={'Contact'} description={''} />} />
+        </Routes>
+        <Footer />
+      </div >
+    </Router>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light ">
-            <a className="navbar-brand" href="#">My Portfolio</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Projects</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Contact</a>
-                    </li>
-                </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    John Tyler Rupert
+                </Link>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/projects">
+                                Projects
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
